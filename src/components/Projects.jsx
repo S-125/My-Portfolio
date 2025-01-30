3c12ff78c21ed5528910fb361097d95c8d8bb32b
S-125 <163549520+S-125@users.noreply.github.com>
2025-01-31 01:05:28 +0530

    Configure deployment for GitHub Pages

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,25 +7,25 @@ const Projects = () => {
     {
       title: 'CookBook',
       description: 'A recipe app where you can find 1000+ dishes to cook everyday.',
-      imageUrl: '/recipe.jpg',
+      imageUrl: '/My-Portfolio/recipe.jpg',
       projectLink: 'https://s-125.github.io/CookBook/',
     },
     {
       title: 'WordNest',
       description: 'A dictionary built to provide meanings along with examples for better understanding.',
-      imageUrl: '/dict.jpg',
+      imageUrl: '/My-Portfolio/dict.jpg',
       projectLink: 'https://s-125.github.io/WordNest-Dictionary/',
     },
     {
       title: 'Tic-Tac-Toe',
       description: 'A simple game of tic-tac-toe for two players.',
-      imageUrl: '/Tic-tac-toe.jpg',
+      imageUrl: '/My-Portfolio/Tic-tac-toe.jpg',
       projectLink: 'https://s-125.github.io/Tic-Tac-Toe/',
     },
     {
         title: 'Note-It',
         description: 'TO-DO LIST that allows the users to add,delete and arrange their tasks as per their priorities.',
-        imageUrl: '/note-it.jpg',
+        imageUrl: '/My-Portfolio/note-it.jpg',
         projectLink: 'https://s-125.github.io/Note-It/',
       },
   ];
